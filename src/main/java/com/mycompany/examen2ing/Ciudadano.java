112428965e98dbea5067e34f27345ca2ae42be12
chichoinf@gmail.com <chichoinf@gmail.com>
2026-06-13 18:55:26 -0300

    clase ciudadano

--- src/main/java/com/mycompany/examen2ing/Ciudadano.java
+++ src/main/java/com/mycompany/examen2ing/Ciudadano.java
@@ -9,44 +9,42 @@ package com.mycompany.examen2ing;
  *
  * @author Vasquez Emmanuel
  */
+
 public class Ciudadano {
+
     private String nombre;
     private String email;
-    private int password;
+    private String password;
 
     public Ciudadano() {}
 
-    public Ciudadano(String nombre, String email, int password) {
+    public Ciudadano(String nombre, String email, String password) {
         this.nombre = nombre;
         this.email = email;
         this.password = password;
     }
 
-    public boolean validarIngreso(int psw) {
-        return this.password == psw;
+    public boolean validarIngreso(String psw) {
+        return this.password.equals(psw);
     }
 
     public void cambioPassword(String newPass) {
-        this.password = Integer.parseInt(newPass);
-    }
-
-    public void delay(int mili) {
-        try {
-            Thread.sleep(mili);
-        } catch (InterruptedException e) {
-            System.out.println("Delay de " + mili + " milisegundos");
-        }
+        this.password = newPass;
     }
 
     public boolean ciudadanosDiferentes(Ciudadano c2) {
         return !this.email.equals(c2.email);
     }
 
-    public int getPassword() {
-        return password;
+    public String getNombre() {
+        return nombre;
     }
 
     public String getEmail() {
         return email;
+    }
+
+    public String getPassword() {
+        return password;
     }
 }

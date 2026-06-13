35f17733063e4da675048c4fab1498aa560bb41f
chichoinf@gmail.com <chichoinf@gmail.com>
2026-06-13 19:25:19 -0300

    clase main

--- src/main/java/com/mycompany/examen2ing/Examen2ing.java
+++ src/main/java/com/mycompany/examen2ing/Examen2ing.java
@@ -11,7 +11,21 @@ package com.mycompany.examen2ing;
 
 public class Examen2ing {
     public static void main(String[] args) {
-        System.out.println("Proyecto Examen2ing funcionando correctamente 🚀");
+
+        SistemaObrasPublicas sistema = new SistemaObrasPublicas();
+
+        Ciudadano c = new Ciudadano("Juan", "juan@mail.com", "1234");
+        Bache b = new Bache(1, "A", "B", 10, "Centro", 5);
+
+        sistema.registrarCiudadano(c);
+        sistema.agregarBache(b);
+
+        InformeRotura inf = sistema.generarInforme(b, c);
+
+        PedidoObra pedido = inf.generarPedido();
+
+        System.out.println("Sistema funcionando correctamente 🚀");
+        System.out.println("Pedido generado en ubicación: " + pedido.getUbicacion());
     }
 }
 
